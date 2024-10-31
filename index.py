121de254150f1d84683dc67d81128698801dab25
Adarsh Kumbhar <adarshkumbhar833@gmail.com>
2024-10-31 13:00:16 +0530

    wsgi updates

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-# from wsgi import app
+from wsgi import app
 
-from app import app
+# from app import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
--- wsgi.py
+++ wsgi.py
@@ -7,8 +7,15 @@
 #     httpd = simple_server.make_server(host, port, app)
 #     httpd.serve_forever()
 
+# from wsgiref import simple_server
+# from app import app
+
+# httpd = simple_server.make_server('0.0.0.0', 5000, app)
+# httpd.serve_forever()
+
 from wsgiref import simple_server
 from app import app
 
-httpd = simple_server.make_server('0.0.0.0', 5000, app)
-httpd.serve_forever()
+if __name__ == "__main__":
+    httpd = simple_server.make_server('0.0.0.0', 5000, app)
+    httpd.serve_forever()
